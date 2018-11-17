05d98a9427dafbc0a76a5986104451113814c92a
Jarren San Jose <sanjose.jarren@gmail.com>
2018-11-17 15:48:05 -0800

    add todos

--- react-hooks-todo/src/App.js
+++ react-hooks-todo/src/App.js
@@ -9,6 +9,29 @@ function Todo({todo, index}) {
   )
 }
 
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add Todo..."
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  )
+}
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -25,12 +48,18 @@ function App() {
     }
   ])
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
